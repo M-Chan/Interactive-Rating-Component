ad8ae2c1b3e785ebb91725d6bc822b393e7b3ed1
M-Chan <mantingchan2@gmail.com>
2022-08-29 14:46:38 +0100

    rating butttons logs a rating and shows 'active'

--- app/app.js
+++ app/app.js
@@ -2,14 +2,28 @@
 let ratingCard = document.querySelector("#ratingContainer");
 let thanksCard = document.querySelector("#thankyouContainer");
 const submitButton = document.querySelector("#submitButton");
+let ratingButtons = document.querySelectorAll(".ratingButton");
+let rating = 0;
 
 submitButton.addEventListener("click", () => submit());
 
 function submit(){
-    console.log("clicked")
+    // console.log("Submitted")
     ratingCard.classList.remove("is--visible");
     ratingCard.classList.add("is--hidden");
 
     thanksCard.classList.remove("is--hidden");
     thanksCard.classList.add("is--visible");
 }
+
+ratingButtons.forEach(mainRatingButton => {
+    mainRatingButton.addEventListener('click', () => {
+    //   console.log("Rating button clicked");
+      ratingButtons.forEach(otherRatingButtons => {
+        otherRatingButtons.classList.remove("ratingButton--active");
+      })
+      mainRatingButton.classList.add("ratingButton--active");
+      rating = mainRatingButton.id;
+    //   console.log("rating:", rating);
+    })
+})
